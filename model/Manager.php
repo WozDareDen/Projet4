88dd03e579fd800a3dd98060bcc86d57ea76beb2
Florent SADOT <fsadot@561557n.ac-rennes.fr>
2018-03-23 12:12:56 +0100

    Online & fixed issues
    -Everything but HomeView on iOS

--- model/Manager.php
+++ model/Manager.php
@@ -3,7 +3,8 @@ class Manager
 {
     protected function dbConnect()
     {
-        $db = new PDO('mysql:host=localhost;dbname=gretaxao_florentsp4;charset=utf8', 'gretaxao_florents', 'florents2017',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        // $db = new PDO('mysql:host=localhost;dbname=gretaxao_florentsp4;charset=utf8', 'gretaxao_florents', 'florents2017',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        $db = new PDO('mysql:host=localhost;dbname=rochefort;charset=utf8', 'root', '',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
         return $db;
     }
 }
